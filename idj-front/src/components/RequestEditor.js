f12e1c6b66437cdbe523edf93aba40899a65abdf
ceeceeS <ocila2@yahoo.fr>
2018-01-31 21:59:28 +0100

    Filter query By cars color

--- idj-front/src/components/RequestEditor.js
+++ idj-front/src/components/RequestEditor.js
@@ -9,10 +9,13 @@ import RequestResult from './RequestResult'
 import Graph from '../js/graphd3'
 import * as d3 from 'd3';
 import '../styles/graph.css'
+import PropTypes from 'prop-types';
 
 class RequestEditor extends Component {
+
    constructor() {
     super()
+    
     // initialize your options array on your state
     this.state = {
       options: [],
@@ -20,8 +23,10 @@ class RequestEditor extends Component {
       selectCarcolor:'',
       showing : false
     }
-  }
 
+
+  
+  }
   onChange(e) {
     // current array of options
     const options = this.state.options
@@ -30,22 +35,12 @@ class RequestEditor extends Component {
     let index;
     // check if the check box is checked or unchecked
    if (e.target.checked && e.target.value !="cars") {
-    //if (e.target.checked && e.target.name == "cars") {
-       // add the numerical value of the checkbox to options array
-       //cars.push(e.target.value)
-       //options[0].push(cars)
-       //console.log(cars)
-     
        options.push(e.target.value)
-       // userItem = (user,i)=> key={i}> for (option in options) {{user.option},}
-    //}else if(e.target.checked && e.target.name != "cars"){
-      //options.push(e.target.value)
     } else {
       // or remove the value from the unchecked checkbox from the array
       index = options.indexOf(e.target.value)
       options.splice(index, 1)
     }
-
     // update the state with the new array of options
     this.setState({ options: options})
    
@@ -74,8 +69,6 @@ class RequestEditor extends Component {
     // update the state with the new array of optionsCars
     this.setState({ optionsCars: optionsCars})
     //this.setState({ selectCarcolor:e.target.value})
-    
-
     //console.log("coleur "+this.state.selectCarcolor);
   
     console.log(this.state.optionsCars);
@@ -112,88 +105,19 @@ onChangeCarColor(e){
     
   }
 
-  displayData(){
-    //display data in function of labels selected
-    var cols = [];
-    var rows = [];
-    var result = [[]];
-    //var dataset = [];
-    //var json = [];
-
-    var optionsCars = this.state.optionsCars;
-    //console.log(optionsCars)
-    this.state.options.map(function(title){
-      cols.push(title);
-    })
-
-    this.props.data.getAllUsers.map(function(row){
-      rows.push(row);
-    })
-    console.log(rows)
-
-    var thead = cols.map((col) => col =="cars"?
-        optionsCars.map((carItem,j) => <th key={j}>Cars.{carItem}</th>): 
-        <th key={col}>{col}</th>
-    );
-
-    var tbody = rows.map(function(row,i){
-        //dataset.push(row);
-        //json.push(JSON.stringify(row));
-        return (
-          <tr key={i}>
-            {cols.map((col,index)=> col =="cars"? /*row[col].length == 0? <td key={index}>test</td>:*/
-            row[col].map((car_label)=>
-              optionsCars.map((carItem,k) => <td key={k}>{car_label[carItem]}</td>)):
-              <td key={index}>{row[col]}</td>)}
-          </tr>
-        );
-    })
-    console.log(rows)
-    //console.log(json);
-
-    return <RequestResult tbody={tbody} thead={thead} datagraph={rows}/>;
-  }
-
   render() {
-    if (this.props.data.loading) {
-            return <div>Loading</div>;
-        }
+   
+    console.log("jdjdj"+this);
    // const selectCarcolor = this.props.selectedoptions
    const {showing} = this.state;
    const {selectCarcolor} = this.state;
-   console.log({selectCarcolor})
-
+  console.log("ehehhe "+ this.state.selectCarcolor);
     return (
       <div className="row">
         <div className="col-xs-12 col-sm-4 col-md-4 col-lg-3">
           <div className = "editor">
-            <form name ="myform">
-                <h3> Users</h3>
-                  <div className = "editor__item">
-                    <label>Full Name</label>
-                    <input type="checkbox"  value= "name" onChange={this.onChange.bind(this)} />
-                  </div>
-                  <div className = "editor__item">
-                    <label>Email</label>
-                    <input type="checkbox" value="email" onChange={this.onChange.bind(this)} />
-                  </div>
-                  <div className = "editor__item">
-                    <label>Age</label>
-                    <input type="checkbox" value="age" onChange={this.onChange.bind(this)} />
-                  </div>
-                  <div className = "editor__item">
-                    <label>Salary</label>
-                    <input type="checkbox" value= "salary" onChange={this.onChange.bind(this)} />
-                  </div>
-                  <div className = "editor__item">
-                    <label>Household</label>
-                    <input type="checkbox" value= "household" onChange={this.onChange.bind(this)} />
-                  </div>
-                 
-              </form>
-              {}
-            </div>
-            <div className = "editor">
+
+          <div className = "editor">
               <form name ="myform">
               <div className="form-group">
                 <div className = "editor_title">
@@ -204,17 +128,9 @@ onChangeCarColor(e){
                     <input type="checkbox"  value="cars" onChange={this.displayCarOption.bind(this) } />
                     {this.state.showing?
                     [
-                    <div className = "editor__item">
-                        <label>Model</label>
-                        <input type="checkbox" name="cars" value="model" onChange={this.onChangeCar.bind(this)} />
-                    </div>,
-                    <div className = "editor__item">
-                        <label>Registration N° </label>
-                        <input type="checkbox" name="cars" value="registrationNo" onChange={this.onChangeCar.bind(this)} />
-                    </div>,
-                    <div className = "editor__item">
+                      <div className = "editor__item">
                     <label>Color</label>
-                    <select value={this.state.selectCarcolor} onChange={this.onChangeCarColor.bind(this)}>
+                    <select name="cars" value={this.state.selectCarcolor} onChange={this.onChangeCarColor.bind(this)}>
                       <option value="select car color">select car color</option>
                       <option value="Red">Red</option>
                       <option value="Black">Black</option>
@@ -224,6 +140,15 @@ onChangeCarColor(e){
                       <option value="Blue">Blue</option>
                     </select>
                 </div>,
+                    <div className = "editor__item">
+                        <label>Model</label>
+                        <input type="checkbox" name="cars" value="model" onChange={this.onChangeCar.bind(this)} />
+                    </div>,
+                    <div className = "editor__item">
+                        <label>Registration N° </label>
+                        <input type="checkbox" name="cars" value="registrationNo" onChange={this.onChangeCar.bind(this)} />
+                    </div>,
+                    
                 <div className = "editor__item">
                     <label>Insurance Price</label>
                     <input type="checkbox" name="cars" value="insurancePrice" onChange={this.onChangeCar.bind(this)} />
@@ -235,52 +160,96 @@ onChangeCarColor(e){
             <div className = "editor__item">
                 <label>Year of manufacture </label>
                 <input type="checkbox" name="cars" value="manufactureYear" onChange={this.onChangeCar.bind(this)} />
-            </div> 
+            </div> ,
+             <h3> Users</h3>,
+             <div className = "editor__item">
+               <label>Full Name</label>
+               <input type="checkbox"  value= "name" onChange={this.onChange.bind(this)} />
+             </div>,
+             <div className = "editor__item">
+               <label>Email</label>
+               <input type="checkbox" value="email" onChange={this.onChange.bind(this)} />
+             </div>,
+             <div className = "editor__item">
+               <label>Age</label>
+               <input type="checkbox" value="age" onChange={this.onChange.bind(this)} />
+             </div>,
+             <div className = "editor__item">
+               <label>Salary</label>
+               <input type="checkbox" value= "salary" onChange={this.onChange.bind(this)} />
+             </div>,
+             <div className = "editor__item">
+               <label>Household</label>
+               <input type="checkbox" value= "household" onChange={this.onChange.bind(this)} />
+             </div>
                     ]:""}
                 </div>  
               </div>
             </form>
           </div>
+           
+               
+                 
+              
+              
+            </div>
+          
         </div>
 
         <div className="col-xs-12 col-sm-8 col-md-8 col-lg-9">
           <div className="result">
-            { this.displayData() }
+         <RequestResult stateEditor={this.state} color={this.state.selectCarcolor}/>
           </div>
         </div>
       </div>
     )
   }
 }
-const Cquery =  gql` query getAllUsers($color: String!){
-    getAllUsers(color: $color) {
-    name
-    email
-    age
-    salary
-    household 
-    cars{
-      _id
-      model
-      registrationNo
-      carColor 
-      insurancePrice 
-      kilometer 
-      manufactureYear
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Cquery =  gql` query getAllUsers($color: String!){
+//     getAllUsers(color: $color) {
+//     name
+//     email
+//     age
+//     salary
+//     household 
+//     cars{
+//       _id
+//       model
+//       registrationNo
+//       carColor 
+//       insurancePrice 
+//       kilometer 
+//       manufactureYear
       
-    }
-  }
-}
-`
-;
-const datafetch = graphql(Cquery,{
-  // The variable $keyword for the query is computed from the
-  // React props passed to this container.
-  options: props=> ({
-    variables: { color: "Grey"},
-  })
-})
+//     }
+//   }
+// }
+// `
+// ;
+// const datafetch = graphql(Cquery,{
+//   // The variable $keyword for the query is computed from the
+//   // React props passed to this container.
+//   options: {
+//     variables: { color: "this.state.selectCarcolor"},
+    
+//   }
+// })
+//jdjdj
 
 
-export default (datafetch)(RequestEditor);
+export default RequestEditor;
 
--- idj-front/src/components/RequestResult.js
+++ idj-front/src/components/RequestResult.js
@@ -14,9 +14,43 @@ import Graph from '../js/graphd3'
 class RequestResult extends Component {
 
     render() {
+        if (this.props.data.loading) {
+            return <div>Loading</div>;
+        }
         
-        var data = this.props.tbody;
-        var labels = this.props.thead;
+        var cols = [];
+        var rows = [];
+        var result = [[]];
+        //var dataset = [];
+        //var json = [];
+    
+        var optionsCars = this.props.stateEditor.optionsCars;
+        //console.log(optionsCars)
+        this.props.stateEditor.options.map(function(title){
+          cols.push(title);
+        })
+    
+        this.props.data.getAllUsers.map(function(row){
+          rows.push(row);
+        })
+        console.log(rows)
+    
+        var thead = cols.map((col) => col =="cars"?
+            optionsCars.map((carItem,j) => <th key={j}>Cars.{carItem}</th>): 
+            <th key={col}>{col}</th>
+        );
+        var tbody = rows.map(function(row,i){
+            //dataset.push(row);
+            //json.push(JSON.stringify(row));
+            return (
+              <tr key={i}>
+                {cols.map((col,index)=> col =="cars"? /*row[col].length == 0? <td key={index}>test</td>:*/
+                row[col].map((car_label)=>
+                  optionsCars.map((carItem,k) => <td key={k}>{car_label[carItem]}</td>)):
+                  <td key={index}>{row[col]}</td>)}
+              </tr>
+            );
+        })
 
         const RawData = () => (
             <div className="result__tablescroll">
@@ -24,11 +58,11 @@ class RequestResult extends Component {
                     <table>
                         <thead>
                             <tr>
-                                {labels}
+                                {thead}
                             </tr>
                         </thead>
                         <tbody>
-                            {data}
+                            {tbody}
                         </tbody>
                     </table>
                 </div>
@@ -44,14 +78,45 @@ class RequestResult extends Component {
                     </div>
 
                     <Route exact path="/dashboard" component={RawData}/>
-                    <Route exact path="/dashboard/graph" render={() => (<Graph data={this.props.datagraph}/>)}/>
+                    <Route exact path="/dashboard/graph" render={() => (<Graph data={rows}/>)}/>
                 </div>
             </Router>
         );
     }
 }
 
-export default RequestResult
+
+const Cquery =  gql` query getAllUsers($color: String!){
+    getAllUsers(color: $color) {
+    name
+    email
+    age
+    salary
+    household 
+    cars{
+      _id
+      model
+      registrationNo
+      carColor 
+      insurancePrice 
+      kilometer 
+      manufactureYear
+      
+    }
+  }
+}
+`
+;
+const datafetch = graphql(Cquery,{
+  // The variable $keyword for the query is computed from the
+  // React props passed to this container.
+  options: (props)=> ({
+    variables: { color:props.color},
+    
+  })
+})
+
+export default(datafetch)(RequestResult)
 
 
 
